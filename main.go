3f656a896c41b33a09562350f6d47c3aea6ca3cd
csmccreery <cameron@mccreery.dev>
2026-06-06 18:25:16 -0400

    Handlers for metric struct added

--- main.go
+++ main.go
@@ -9,29 +9,57 @@ import (
 
 type apiConfig struct {
 	fileServerHits atomic.Int32
+	isOk bool
 }
 
 func (cfg *apiConfig) middleWareMetricsInc(next http.Handler) http.Handler {
-	cfg.fileServerHits.Add(1)
-	return next
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileServerHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
 }
 
-func healthHandler(w http.ResponseWriter, r *http.Request) {
-	if r.Method != "GET" {
-		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
-		return
-	}
+func (cfg *apiConfig) handleMetrics(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-	w.WriteHeader(200)
-	w.Write([]byte("OK"))
+	w.WriteHeader(http.StatusOK)
+	
+	fmt.Fprintf(w, "Hits: %d", cfg.fileServerHits.Load())
+}
+
+
+func (cfg *apiConfig) resetMetrics(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+
+	cfg.fileServerHits.Store(0)
+}
+
+func (cfg *apiConfig) healthHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	if cfg.isOk {
+		w.WriteHeader(http.StatusOK)
+		fmt.Fprintf(w, "OK")
+	} else {
+		w.WriteHeader(503)
+		fmt.Fprintf(w, "Not OK")
+	}
 }
 
 func main() {
 	servMux := http.NewServeMux()
-	servMux.Handle("/app/", http.StripPrefix("/app/", http.FileServer(http.Dir("."))))
-	servMux.Handle("/app/assets/", http.FileServer(http.Dir(".")))
+	
+	cfg := &apiConfig{}
+	cfg.isOk = true
 
-	servMux.HandleFunc("/healthz", healthHandler)
+	fileServer := http.FileServer(http.Dir("."))
+
+	servMux.Handle("/app/", http.StripPrefix("/app/", cfg.middleWareMetricsInc(fileServer)))
+	servMux.Handle("/app/assets", http.StripPrefix("/app/", cfg.middleWareMetricsInc(fileServer)))
+	
+	servMux.HandleFunc("/metrics", cfg.handleMetrics)
+	servMux.HandleFunc("/reset", cfg.resetMetrics)
+	servMux.HandleFunc("/healthz", cfg.healthHandler)
+	
 	
 	fmt.Printf("Listening on port 8080")
 	log.Fatal(http.ListenAndServe(":8080", servMux))
