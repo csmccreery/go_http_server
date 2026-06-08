61e5bea8a31789c2dff77fa061eb9892f070c70d
csmccreery <cameron@mccreery.dev>
2026-06-07 21:42:59 -0400

    Migrating to container workflow to support postgres

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/go_http_server
 
 go 1.25.10
+
+require (
+	github.com/satori/go.uuid v1.2.0 // indirect
+	github.com/satori/uuid.go v1.2.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,25 +1,58 @@
 package main
 
 import (
+	"strings"
+	"time"
 	"fmt"
 	"sync/atomic"
 	"log"
 	"net/http"
+	"encoding/json"
+	"github.com/satori/uuid.go"
 )
 
-type apiConfig struct {
+type ApiConfig struct {
 	fileServerHits atomic.Int32
-	isOk bool
+	Ok bool
 }
 
-func (cfg *apiConfig) middleWareMetricsInc(next http.Handler) http.Handler {
+type Content struct {
+	Body string
+}
+
+type Chirp struct {
+	U1 string
+	Length int
+	Timestamp time.Time
+	Content Content
+}
+
+func generateUUID() (string) {
+	u1 := uuid.NewV4()
+	return u1.String()
+}
+
+func cleanProfaneWords(chirp string, profaneMap map[string]bool) string {
+	fields := strings.Fields(chirp)
+	for i:=0; i<len(fields); i++ {
+		word := strings.ToLower(fields[i])
+		_, ok := profaneMap[word]
+		if ok {
+			fields[i] = "****"
+		}
+	}
+
+	return strings.Join(fields, " ")
+}
+
+func (cfg *ApiConfig) middleWareMetricsInc(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		cfg.fileServerHits.Add(1)
 		next.ServeHTTP(w, r)
 	})
 }
 
-func (cfg *apiConfig) handleMetrics(w http.ResponseWriter, r *http.Request) {
+func (cfg *ApiConfig) handleMetrics(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
 	
@@ -27,16 +60,16 @@ func (cfg *apiConfig) handleMetrics(w http.ResponseWriter, r *http.Request) {
 }
 
 
-func (cfg *apiConfig) resetMetrics(w http.ResponseWriter, r *http.Request) {
+func (cfg *ApiConfig) resetMetrics(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
 
 	cfg.fileServerHits.Store(0)
 }
 
-func (cfg *apiConfig) healthHandler(w http.ResponseWriter, r *http.Request) {
+func (cfg *ApiConfig) healthHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-	if cfg.isOk {
+	if cfg.Ok {
 		w.WriteHeader(http.StatusOK)
 		fmt.Fprintf(w, "OK")
 	} else {
@@ -45,36 +78,66 @@ func (cfg *apiConfig) healthHandler(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-func (cfg *apiConfig) validateChirp(w http.ResponseWriter, r *http.Request) {
-	type content struct {
-		body string 'json:"body"'
-	}
-
-	contentVal := content{}
-
-	dat, err := json.Marshal(contentVal)
+func (cfg *ApiConfig) respondWithJSON(w http.ResponseWriter, code int, payload any) {
+	data, err := json.Marshal(payload)
 	if err != nil {
-		log.Printf("Error marshalling JSON: %s", err)
+		log.Printf("Failed to marshal json", err)
 		w.WriteHeader(500)
 		return
 	}
 
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
-	fmt.Fprintf(w, "%s", dat)
+	fmt.Fprintf(w, string(data))
 }
 
+func (cfg *ApiConfig) respondWithError(w http.ResponseWriter, code int, msg string) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(400)
+}
+
+func (cfg *ApiConfig) validateChirp(w http.ResponseWriter, r *http.Request) {
+	content := Content{}
+
+	decoder := json.NewDecoder(r.Body)
+	err := decoder.Decode(&content)
+	if err != nil {
+		log.Printf("Error decoding chirp content: %s", err)
+		w.WriteHeader(500)
+		return
+	}
+
+	if (len(content.Body) > 140) {
+		cfg.respondWithError(w, 400, "Chirp is too long")
+	} else {
+		profaneMap := map[string]bool{
+			"kerfuffle": true,
+			"sharbert": true,
+			"fornax": true,
+		}
+		cleanedBody := cleanProfaneWords(content.Body, profaneMap)
+		
+		type Payload struct {
+			Valid bool `json:"valid"`
+			Cleaned string `json:"cleaned_body"`
+		}
+
+		payload := Payload{Valid: true, Cleaned: cleanedBody}
+		cfg.respondWithJSON(w, 200, payload)
+	}
+}
+	
 func main() {
 	servMux := http.NewServeMux()
 	
-	cfg := &apiConfig{}
-	cfg.isOk = true
+	cfg := &ApiConfig{}
+	cfg.Ok = true
 
 	fileServer := http.FileServer(http.Dir("."))
 
 	servMux.Handle("/app/", http.StripPrefix("/app/", cfg.middleWareMetricsInc(fileServer)))
 	servMux.Handle("/app/assets", http.StripPrefix("/app/", cfg.middleWareMetricsInc(fileServer)))
-	servMux.HandleFunc("POST /api/validate_chirp" cfg.validateChirp)
+	servMux.HandleFunc("POST /api/validate_chirp", cfg.validateChirp)
 	servMux.HandleFunc("GET /admin/metrics", cfg.handleMetrics)
 	servMux.HandleFunc("POST /admin/reset", cfg.resetMetrics)
 	servMux.HandleFunc("GET /api/healthz", cfg.healthHandler)
